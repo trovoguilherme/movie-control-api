8afa82ce0c1537ea148b1c2a204c16c4c667893f
trovo <guinavy2011@gmail.com>
2024-02-08 20:41:26 -0300

    Adicionando configuração de log do CircuitBraker

---
+++ src/main/java/br/com/ovort/configs/CircuitBreakerLogConfig.java
@@ -0,0 +1,40 @@
+package br.com.ovort.configs;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.core.registry.EntryAddedEvent;
+import io.github.resilience4j.core.registry.EntryRemovedEvent;
+import io.github.resilience4j.core.registry.EntryReplacedEvent;
+import io.github.resilience4j.core.registry.RegistryEventConsumer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class CircuitBreakerLogConfig {
+
+    @Bean
+    public RegistryEventConsumer<CircuitBreaker> cbLog() {
+        return new RegistryEventConsumer<CircuitBreaker>() {
+            @Override
+            public void onEntryAddedEvent(EntryAddedEvent<CircuitBreaker> entryAddedEvent) {
+                entryAddedEvent.getAddedEntry()
+                        .getEventPublisher()
+                        .onStateTransition(event -> {
+                            log.info(event.toString());
+                        });
+            }
+
+            @Override
+            public void onEntryRemovedEvent(EntryRemovedEvent<CircuitBreaker> entryRemoveEvent) {
+
+            }
+
+            @Override
+            public void onEntryReplacedEvent(EntryReplacedEvent<CircuitBreaker> entryReplacedEvent) {
+
+            }
+        };
+    }
+
+}
