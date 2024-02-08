80499fbfaf9aa5102f9f67d910bf8653fc7e6ea5
trovo <guinavy2011@gmail.com>
2024-02-07 23:46:09 -0300

    Criação de busca por titulo

--- src/main/java/br/com/ovort/repository/FilmeRepository.java
+++ src/main/java/br/com/ovort/repository/FilmeRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface FilmeRepository extends JpaRepository<Filme, Integer> {
 
     List<Filme> findAllByUser(User user);
+
+    Optional<Filme> findByTitulo(String title);
 
 }
